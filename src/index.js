04407c4b4d13a9ac5e23c28d7b6469182696c920
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-06-30 15:10:58 +0500

    last changes v2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import store from './context/store';
-import { BrowserRouter } from 'react-router-dom'
+
+import store from "./context/store";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
