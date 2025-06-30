13d7f1ff445b12b9caa6d9ac1e9ca4dcfec3898c
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2025-06-30 15:17:44 +0500

    last changes v3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
 import store from "./context/store";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
